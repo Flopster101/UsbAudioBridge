d9c336a3e0da17dd8ab4efae41eed23a6326d0de
Flopster101 <nahuelgomez329@gmail.com>
2026-03-07 15:40:45 -0300

    build: Update SDK targets
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
     defaultConfig {
         applicationId = "com.flopster101.usbaudiobridge"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1000
         versionName = "1.0.0"
 
